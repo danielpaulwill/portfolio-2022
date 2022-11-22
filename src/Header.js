013cc78bb8f830c9118805033f264100d38eb379
Daniel Williams <danielpaulwill@gmail.com>
2022-11-21 20:43:46 -0500

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+
+
+function Header() {
+  return (
+    <div className="row">
+      <div className="col-3 separator">x</div>
+      <div className="col-3 separator">
+        <h1>Header</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
