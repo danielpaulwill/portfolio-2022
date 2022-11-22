e3cb94bf0a8bb710a05c92d0f3a163a3678f1ead
Daniel Williams <danielpaulwill@gmail.com>
2022-11-21 21:23:23 -0500

    navbar set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,33 @@
 
 
 function Header() {
-  return (
-    <div className="row">
-      <div className="col-3 separator">x</div>
-      <div className="col-3 separator">
-        <h1>Header</h1>
+  return (  
+    <nav class="navbar navbar-expand-lg bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand text-white" href="#">Daniel Williams</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Projects</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Blogs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Contact</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+          </form>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
