66ace9d95721c07ac64d1cfe1c2b7ea57e9ef5d5
Daniel Williams <danielpaulwill@gmail.com>
2022-12-02 22:32:33 -0500

    home menu buttons on page

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,16 @@ function Home() {
   return (
     <div>
       <img src="https://raw.githubusercontent.com/danielpaulwill/portfolio-2022/main/src/assets/stretched.jpg" class="img-fluid" alt="Daniel Williams"></img>
+      <div className="row">
+        <div className="col-md-3 red"></div>
+        <div className="col-md-6 buttonMenu">
+          <button type="button" class="btn btn-outline-dark btn-lg menuButtonLeft">Projects</button>
+          <button type="button" class="btn btn-outline-dark btn-lg menuButtonMiddle">Blogs</button>
+          <button type="button" class="btn btn-outline-dark btn-lg menuButtonMiddle">Contact</button>
+          <button type="button" class="btn btn-outline-dark btn-lg menuButtonRight">Resume</button>
+        </div>
+        <div className="col-md-3 red"></div>
+      </div>
     </div>
   );
 }
