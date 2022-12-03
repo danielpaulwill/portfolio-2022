a6f12416c701d5a17d966d24ebf11f803e00ec1b
Daniel Williams <danielpaulwill@gmail.com>
2022-12-03 00:26:27 -0500

    project slides integrated

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,60 @@
 function Home() {
   return (
     <div>
-      <img src="https://raw.githubusercontent.com/danielpaulwill/portfolio-2022/main/src/assets/stretched.jpg" class="img-fluid" alt="Daniel Williams"></img>
+      <img src="https://raw.githubusercontent.com/danielpaulwill/portfolio-2022/main/src/assets/stretched.jpg" className="img-fluid" alt="Daniel Williams"></img>
       <div className="row">
-        <div className="col-md-3 red"></div>
+        <div className="col-md-3"></div>
         <div className="col-md-6 buttonMenu">
-          <button type="button" class="btn btn-outline-dark btn-lg menuButtonLeft">Projects</button>
-          <button type="button" class="btn btn-outline-dark btn-lg menuButtonMiddle">Blogs</button>
-          <button type="button" class="btn btn-outline-dark btn-lg menuButtonMiddle">Contact</button>
-          <button type="button" class="btn btn-outline-dark btn-lg menuButtonRight">Resume</button>
+          <button type="button" className="btn btn-outline-dark btn-lg menuButtonLeft">Projects</button>
+          <button type="button" className="btn btn-outline-dark btn-lg menuButtonMiddle">Blogs</button>
+          <button type="button" className="btn btn-outline-dark btn-lg menuButtonMiddle">Contact</button>
+          <button type="button" className="btn btn-outline-dark btn-lg menuButtonRight">Resume</button>
         </div>
-        <div className="col-md-3 red"></div>
+        <div className="col-md-3"></div>
+      </div>
+      <div className="row">
+        <div className="col-md-2"></div>
+        <div className="col-md-8">
+          <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+            <div className="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src="https://raw.githubusercontent.com/danielpaulwill/portfolio-2022/main/src/assets/stretched.jpg" className="d-block w-100" alt="..."></img>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>Some representative placeholder content for the first slide.</p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img src="https://raw.githubusercontent.com/danielpaulwill/portfolio-2022/main/src/assets/stretched.jpg" className="d-block w-100" alt="..."></img>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img src="https://raw.githubusercontent.com/danielpaulwill/portfolio-2022/main/src/assets/stretched.jpg" className="d-block w-100" alt="..."></img>
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>Third slide label</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+        <div className="col-md-2"></div>
       </div>
     </div>
   );
