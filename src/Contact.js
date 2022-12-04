de03b02ee43a3b3b7dd96d060dabeee086b5a3aa
Daniel Williams <danielpaulwill@gmail.com>
2022-12-03 19:04:56 -0500

    social icons added

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
         <div className="col-md-2"></div>
         <div className="col-md-8 containerSpace">
         <button type="button" class="btn btn-dark">Dark</button>
-
+        <img src="https://raw.githubusercontent.com/danielpaulwill/portfolio-2022/14ea929253bedcb28b8613faa285683b2f81b511/src/assets/LinkedIn%20Icon.svg"></img>
         </div>
         <div className="col-md-2"></div>
       </div>
