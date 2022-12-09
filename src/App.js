2dad6e3ed2792becd455c6591cd7c52f28ba4ca2
Daniel Williams <danielpaulwill@gmail.com>
2022-12-09 15:46:21 -0500

    added images, working on array of projects

--- src/App.js
+++ src/App.js
@@ -6,11 +6,28 @@ import Projects from './Projects';
 import Blogs from './Blogs';
 
 function App() {
+
+  const projectInfo = [
+    {
+      id: 1,
+      title: "Starbucks Confidential",
+      subtitle: "An informational app that brings you the unlisted caffeine content of popular bottled Starbucks drinks.",
+      image: ""
+    },
+    {
+      id: 2,
+      title: "Wayfarer: Journey to Understanding",
+      subtitle: "An 8-bit multiple choice mystery RPG with some shocking twists and turns.",
+      image: ""
+    },
+  ]
+
+
   return (
     <div>
       <Header />
       {/* <Blogs /> */}
-      {/* <Home /> */}
+      <Home />
       <Projects />
       {/* <Contact /> */}
     </div>
--- src/Projects.js
+++ src/Projects.js
@@ -5,12 +5,22 @@ function Projects() {
         <div className="col-md-3"></div>
         <div className="col-md-6">
           <div className="card cardSpace">
-            <div class="card-header text-bg-danger">Deployed</div>
+            <div class="card-header">
+              <span class="badge text-bg-primary">Primary</span>
+              <span class="badge text-bg-secondary">Secondary</span>
+              <span class="badge text-bg-success">Success</span>
+              <span class="badge text-bg-danger">Danger</span>
+              <span class="badge text-bg-warning">Warning</span>
+              <span class="badge text-bg-info">Info</span>
+              <span class="badge text-bg-light">Light</span>
+              <span class="badge text-bg-dark">Dark</span>
+            </div>
+            
             <img src="https://raw.githubusercontent.com/danielpaulwill/portfolio-2022/main/src/assets/stretched.jpg" class="" alt="..."></img>
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-dark btnSpace">Video Demo</a>
+              <h5 className="card-title">Starbucks Confidential</h5>
+              <p class="card-text">An informational app that brings you the unlisted caffeine content of popular bottled Starbucks drinks.</p>
+              <a href="#" className="btn btn-dark btnSpace disabled" role="button" aria-disabled="true">Video Demo</a>
               <a href="#" className="btn btn-dark btnSpace">Live Demo</a>
               <a href="#" className="btn btn-dark btnSpace">Repo</a>
             </div>
