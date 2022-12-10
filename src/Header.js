bb01dfd677e32459522d7dfe2333183869ebd917
Daniel Williams <danielpaulwill@gmail.com>
2022-12-09 22:14:24 -0500

    nav styling updated

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,26 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function Header() {
   return (  
-    <nav className="navbar navbar-expand-lg bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand text-white" href="#">Daniel Williams</a>
-        <button className="navbar-toggler toggleButton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">Daniel Williams</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className='nav-link active text-muted' href='/'>Home</a>
+              <a className='nav-link' aria-current='page' href='/'>Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href='projects'>Projects</a>
+              <a className="nav-link" href='projects'>Projects</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="blogs">Blogs</a>
+              <a className="nav-link" href="blogs">Blogs</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="contact">Contact</a>
+              <a className="nav-link" href="contact">Contact</a>
             </li>
           </ul>
           <form className="d-flex" role="search">
