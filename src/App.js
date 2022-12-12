76644b8a90e63ee7a100b049e406e9ceb191c50a
Daniel Williams <danielpaulwill@gmail.com>
2022-12-11 21:03:51 -0500

    fixed known errors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './Home';
 import Projects from './Projects';
 import Blogs from './Blogs';
 import Error from './Error';
-import { BrowserRouter, useNavigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
